75b0aee6345322d71425d1cb285853bcdd8e004a
Dirkster99 <Dirk.Bahle@gmx.de>
2018-08-26 00:00:53 +0200

    Fine tuning the projects in the solution...

---
+++ source/TestGenerics/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TestGenerics
+{
+  using System.Windows;
+  using TestGenerics.ViewModel;
+
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      DataContext = new AppViewModel();
+    }
+  }
+}
