e769eb5c0ef5accb22d9ea4c8915a7905f0c521f
Dirkster99 <Dirk.Bahle@gmx.de>
2018-08-26 02:00:28 +0200

    Adjusted CleanAll.bat and Project NuGet Refs

--- source/CleanAll.bat
+++ source/CleanAll.bat
@@ -6,8 +6,13 @@ ECHO.
 ECHO This script deletes all temporary build files in their
 ECHO corresponding BIN and OBJ Folder contained in the following projects
 ECHO.
-ECHO NumericUpDowmControlDemo
+ECHO TestThemes
+ECHO TestGenerics
 ECHO NumericUpDownLib
+ECHO Demo\UpDownDemoLib
+ECHO Demo\MLibTest\Components\ServiceLocator
+ECHO Demo\MLibTest\Components\Settings\Settings
+ECHO Demo\MLibTest\Components\Settings\SettingsModel
 ECHO.
 REM Ask the user if hes really sure to continue beyond this point XXXXXXXX
 set /p choice=Are you sure to continue (Y/N)?
@@ -18,17 +23,49 @@ ECHO XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 ECHO.
 ECHO XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 ECHO.
-ECHO Deleting BIN and OBJ Folders in NumericUpDowmControlDemo
-ECHO.
-RMDIR /S /Q .\.vs
-RMDIR /S /Q .\NumericUpDowmControlDemo\bin
-RMDIR /S /Q .\NumericUpDowmControlDemo\obj
-ECHO.
 ECHO Deleting BIN and OBJ Folders in NumericUpDownLib
 ECHO.
 RMDIR /S /Q .\NumericUpDownLib\bin
 RMDIR /S /Q .\NumericUpDownLib\obj
 ECHO.
+ECHO Deleting BIN and OBJ Folders in TestThemes
+ECHO.
+RMDIR /S /Q .\TestThemes\bin
+RMDIR /S /Q .\TestThemes\obj
+ECHO.
+ECHO Deleting BIN and OBJ Folders in TestGenerics
+ECHO.
+RMDIR /S /Q .\TestGenerics\bin
+RMDIR /S /Q .\TestGenerics\obj
+ECHO.
+
+ECHO.
+ECHO Deleting BIN and OBJ Folders in UpDownDemoLib
+ECHO.
+RMDIR /S /Q .\.vs
+RMDIR /S /Q .\Demo\UpDownDemoLib\bin
+RMDIR /S /Q .\Demo\UpDownDemoLib\obj
+
+ECHO.
+ECHO Deleting BIN and OBJ Folders in Demo\MLibTest\Components\ServiceLocator
+ECHO.
+RMDIR /S /Q .\.vs
+RMDIR /S /Q .\Demo\MLibTest\Components\ServiceLocator\bin
+RMDIR /S /Q .\Demo\MLibTest\Components\ServiceLocator\obj
+
+ECHO.
+ECHO Deleting BIN and OBJ Folders in Demo\MLibTest\Components\Settings\Settings
+ECHO.
+RMDIR /S /Q .\.vs
+RMDIR /S /Q .\Demo\MLibTest\Components\Settings\Settings\bin
+RMDIR /S /Q .\Demo\MLibTest\Components\Settings\Settings\obj
+
+ECHO.
+ECHO Deleting BIN and OBJ Folders in Demo\MLibTest\Components\Settings\SettingsModel
+ECHO.
+RMDIR /S /Q .\.vs
+RMDIR /S /Q .\Demo\MLibTest\Components\Settings\SettingsModel\bin
+RMDIR /S /Q .\Demo\MLibTest\Components\Settings\SettingsModel\obj
 
 PAUSE
 
