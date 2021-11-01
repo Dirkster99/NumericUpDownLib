0ff0cbfe85d82e7a76cda33f20fb366f08327e11
Dirkster99 <Dirk.Bahle@gmx.de>
2021-11-01 17:19:59 +0100

    Added Hex Display On/Off support for demo application into Byte, Long, Integer, SByte, Short, UInteger, ULong and Ushort Demo views

--- source/Demo/UpDownDemoLib/ViewModels/BaseUpDownViewModel.cs
+++ source/Demo/UpDownDemoLib/ViewModels/BaseUpDownViewModel.cs
@@ -16,6 +16,10 @@ namespace UpDownDemoLib.ViewModels
         private T _StepSize = default(T);
         private T _LargeStepSize = default(T);
         private ModifierKeys _AccelModifierKeys = ModifierKeys.Control;
+
+        private bool _IsHexDisplayEnabled = false;
+        protected string _FormatString = "D";
+        protected string _NumberStyle = "None";
         #endregion fields
 
         #region CTors
@@ -131,6 +135,73 @@ namespace UpDownDemoLib.ViewModels
                 }
             }
         }
+
+		#region HEX DISPLAY ON/OFF Properties
+		/// <summary>Get/set whether value in control should be displayed as normal integer or hex value.</summary>
+		public bool IsHexDisplayEnabled
+        {
+            get
+            {
+                return _IsHexDisplayEnabled;
+            }
+
+            set
+            {
+                if (_IsHexDisplayEnabled != value)
+                {
+                    _IsHexDisplayEnabled = value;
+                    NotifyPropertyChanged(() => IsHexDisplayEnabled);
+
+                    if (value)
+                    {
+                        this.FormatString = "X2";
+                        this.NumberStyle = "HexNumber";
+                    }
+                    else
+                    {
+                        this.FormatString = "D";
+                        this.NumberStyle = "None";
+                    }
+                }
+            }
+        }
+
+        /// <summary>Get/sets the FormatString indicator that defines the format of the value display in the textbox portion.</summary>
+        public string FormatString
+        {
+            get
+            {
+                return _FormatString;
+            }
+
+            set
+            {
+                if (_FormatString != value)
+                {
+                    _FormatString = value;
+                    NotifyPropertyChanged(() => FormatString);
+                }
+            }
+        }
+
+        /// <summary>Get/sets the <seealso cref="System.Globalization.NumberStyles"/> indicator that defines the style of the value display in the textbox portion.</summary>
+        public string NumberStyle
+        {
+            get
+            {
+                return _NumberStyle;
+            }
+
+            set
+            {
+                if (_NumberStyle != value)
+                {
+                    _NumberStyle = value;
+                    NotifyPropertyChanged(() => NumberStyle);
+                }
+            }
+        }
+        #endregion  HEX DISPLAY ON/OFF Properties
         #endregion properties
 
         #region methods
